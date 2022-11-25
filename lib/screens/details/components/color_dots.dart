6b3d6033828f9ca2304e9aeaade06ec9ba83ba06
aubreykamanga <bsc-03-17@cc.ac.mw>
2022-11-25 12:53:01 -0800

    UI complete
    UI with flutter

---
+++ lib/screens/details/components/color_dots.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class ColorDots extends StatelessWidget {
+  const ColorDots({
+    Key? key,
+    required this.fillColor,
+    //by default, isSelected is set to False
+    this.isSelected = false,
+  }) : super(key: key);
+
+  final Color fillColor;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // left and right padding
+      margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding / 2.5),
+      padding: const EdgeInsets.all(3),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected ? const Color(0xFF707070) : Colors.transparent,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: fillColor,
+        ),
+      ),
+    );
+  }
+}
